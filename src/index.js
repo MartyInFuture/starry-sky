045038a6f14e5c24746c0332a5a17ec061945f93
MartyInFuture <martyinfuture@gmail.com>
2021-08-04 21:14:42 +0300

    add stars and lines without normal path length

--- src/index.js
+++ src/index.js
@@ -31,25 +31,65 @@ const starsArr = [];
 function createDots(amount = 10, obj = randomPosition()) {
   let color = config.starColor;
   if (isFirst) {
-    obj.first = true;
-    starsArr.push(obj);
+    obj.position = 'first';
     color = config.firstStarColor;
     isFirst = false;
   }
   if (amount === 1) {
-    obj.last = true;
-    starsArr.push(obj);
+    obj.position = 'last';
+
     color = config.lastStarColor;
   }
   if (amount === 0) return false;
-  console.log(obj);
+  // console.log(obj);
   circle.beginPath();
   circle.arc(obj.width, obj.height, 3, 0, Math.PI * 2);
   circle.fillStyle = color;
   circle.fill();
 
+  obj.average = parseInt((obj.height + obj.width) / 2);
+  obj.isUsed = false;
+  starsArr.push(obj);
+
   createDots(--amount);
 }
 
 createDots();
-console.log(starsArr);
+const lines = canvas.getContext('2d');
+lines.beginPath();
+function createLines(starsArr) {
+  const newArr = [...starsArr];
+  const shortestWayArr = [newArr[0]];
+
+  const newCoordinate = now => {
+    if (now === undefined) return false;
+    console.log(newArr.indexOf(now));
+    now.isUsed = true;
+    let some = 9999;
+    let obj = {};
+    for (let i = 0; i < starsArr.length - 1; i++) {
+      if (
+        Math.abs(now.average - newArr[i].average) < some &&
+        !newArr[i].isUsed &&
+        i != starsArr.length - 1
+      ) {
+        some = Math.abs(now.average - newArr[i].average);
+        obj = newArr[i];
+      }
+    }
+    console.log(some);
+    if (obj.width != undefined) shortestWayArr.push(obj);
+    newCoordinate(newArr[newArr.indexOf(obj)]);
+  };
+  newCoordinate(newArr[0]);
+  shortestWayArr.push(newArr[newArr.length - 1]);
+  for (let i = 0; i < shortestWayArr.length - 1; i++) {
+    lines.moveTo(shortestWayArr[i].width, shortestWayArr[i].height);
+    lines.lineTo(shortestWayArr[i + 1].width, shortestWayArr[i + 1].height);
+    lines.stroke();
+  }
+  console.log(newArr);
+  console.log(shortestWayArr);
+}
+
+createLines(starsArr);
